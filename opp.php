40e8e005abb95b97db2fa06fa983144a15436213
tranmy497 <56033819+tranmy497@users.noreply.github.com>
2019-10-23 19:13:45 +0700

    Add files via upload

---
+++ opp.php
@@ -0,0 +1,15 @@
+<?php
+class User{
+	public $username;
+	public $email;
+	public $password
+	public $phone;
+	public $Address;
+	public function setUserName()
+	{
+	echo "my name";
+	}
+         }
+$user = new User();
+
+?>
